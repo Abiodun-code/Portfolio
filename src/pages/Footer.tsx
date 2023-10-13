aaaf5ef203891ada13a7e189039c03e7b2f71f56
Olaroungbe Jamiu <88253089+Abiodun-code@users.noreply.github.com>
2023-10-13 06:43:40 -0700

    Update Footer

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="block justify-between items-center">
           <div className="block">
             <h1 className="text-white font-primary font-medium text-xl tracking-wider">Email</h1>
-            <a href="mailto:olaabiodun157@gmail.com" className="text-white font-seconday text-lg tracking-wider">olaabiodun157@gmail.com</a>
+            <a href="mailto:olaroungbejamiu@gmail.com" className="text-white font-seconday text-lg tracking-wider">olaroungbejamiu@gmail.com</a>
           </div>
           <div className="flex items-center gap-x-2 justify-between pt-10">
             <h1 className="text-white font-primary font-medium xl:text-xl lg:text-xl text-lg tracking-wider">@Abiodun Code 🌎</h1>
