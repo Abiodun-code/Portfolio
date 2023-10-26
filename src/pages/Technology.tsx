e069936d5cb898cd6c498b938102406536168057
Jamiu Olaroungbe <88253089+Abiodun-code@users.noreply.github.com>
2023-10-26 13:27:43 -0700

    Update Technology.tsx

--- src/pages/Technology.tsx
+++ src/pages/Technology.tsx
@@ -5,7 +5,7 @@ const Technology = () => {
     <div className="w-auto h-auto">
       <div className="container mx-auto xl:max-w-[80%] lg:max-w-[85%] px-3 pt-20 ">
         <div className="block justify-center items-center py-5 text-white text-center">
-          <h1 className='text-5xl font-primary xl:text-[40px] lg:text-[60px] tracking-wider font-extrabold capitalize'>Technologies i use</h1>
+          <h1 className='text-5xl font-primary xl:text-[40px] lg:text-[60px] tracking-wider font-extrabold capitalize'>Technologies i use 👨‍💻</h1>
           <div className="flex flex-wrap justify-center items-center gap-8 pt-8">
             {Framework.map((item, idx)=>(
               <div key={idx} className="items-center  flex justify-center w-12 h-12 object-contain">
